1835b75a23365f6c831bb55f7fd9a9a4c63d00fa
Hussein Al Fartoussi <114392226+husseinALF@users.noreply.github.com>
2022-11-10 12:20:12 +0100

    fetch json file

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ hamburger.addEventListener('click', () => {
 
     mobile_menu.classList.toggle('is-open');
 })
+
+let datalist = document.querySelector(".cv-data");
+let joblist = document.querySelector(".cv-jobs")
+
+let url = "./data.json";
+
+async function getcv() {
+    const response = await fetch(url)
+        .then((response) => response.json())
+        .then((response) => (data = response));
+    data.educations.forEach(element => {
+        let li = document.createElement("li");
+        li.style.listStyle = "none";
+        li.innerHTML = ` ${element.highschool} <br> ${element.focus} <br> ${element.year}`;
+        datalist.appendChild(li)
+    });
+
+    data.work.forEach(element => {
+        let li = document.createElement("li");
+        li.style.listStyle = "none";
+        li.innerHTML = `${element.job}  ${element.year}`
+        joblist.appendChild(li)
+    })
+
+
+
+}
+getcv()
+
