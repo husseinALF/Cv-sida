7edc87a7af11f50cd0b2ddf766fe0c6c4c4acc3f
Hussein Al Fartoussi <114392226+husseinALF@users.noreply.github.com>
2022-11-17 20:12:03 +0100

    Putting repo list

--- githubAPI.js
+++ githubAPI.js
@@ -1,7 +1,8 @@
 let h4 = document.querySelector(".project-github-h4");
 let p = document.querySelector(".project-github-desc");
-let loader = document.querySelector(".loading")
-
+let ul = document.querySelector(".project-ul");
+let loader = document.querySelector(".loading");
+let loader2 = document.querySelector(".loading2");
 let url = "https://api.github.com/users/husseinALF/repos"
 
 async function getapi() {
@@ -9,11 +10,17 @@ async function getapi() {
         .then((response) => response.json())
         .then((response) => (data = response));
     loader.innerText = "";
+    loader2.innerText = null;
     data.forEach(element => {
         h4.innerHTML = element.name;
     });
     data.forEach(element => {
         p.innerHTML = element.description;
+    });
+    data.forEach(element => {
+        let li = document.createElement("li");
+        ul.appendChild(li)
+        li.innerHTML = element.name;
     });
 }
 
